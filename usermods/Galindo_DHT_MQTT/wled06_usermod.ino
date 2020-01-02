9b3734bd2162809fcf29751bed2f2c76de720b94
Galindo <magalindo8@gmail.com>
2020-01-02 23:41:48 +0100

    debugs añadidos y pin boton a -1 para poder usar D3 con el pull up

--- usermods/Galindo_DHT_MQTT/wled06_usermod.ino
+++ usermods/Galindo_DHT_MQTT/wled06_usermod.ino
@@ -1,17 +1,18 @@
 #include "DHT.h"
-//#define WLED_DEBUG_DHT //uncoment this line to see the DHT debugs 
+#define DHT_DEBUG //uncoment this line to see the DHT debugs 
 #define DHTTYPE DHT22
 long lastMeasure = 0;
 long temptimer = 0;
-int delayMs = 10000; //10 sec
+int delayMs = 2000; //2 sec
 const int DHTPIN = D3; //DHT data pin
 DHT dht(DHTPIN, DHTTYPE);
 
 //starts Dallas Temp service on boot
 void userSetup(){
-  pinMode(DHTPIN, INPUT_PULLUP);
-  //Start the DHT sensor
-  dht.begin();
+    Serial.print("DHT setup");
+    pinMode(DHTPIN, INPUT_PULLUP);
+    //Start the DHT sensor
+    dht.begin();
 }
     
 //gets called every time WiFi is (re-)connected. Initialize own network interfaces here
@@ -23,6 +24,7 @@ void userLoop(){
   
   //Timer to publishe new temperature every 10 seconds
   if (temptimer - lastMeasure > delayMs) {
+    Serial.println("codigo DHT");
     lastMeasure = temptimer;
     
     //Check if MQTT Connected, otherwise it will crash the 8266
@@ -30,12 +32,17 @@ void userLoop(){
       //DHT readings
       float dht_humidity = dht.readHumidity();
       float dht_temperature = dht.readTemperature();
+      Serial.print("hum: ");
+      Serial.println(dht_humidity);
+      Serial.print("temp: ");
+      Serial.println(dht_temperature);
 
       if (isnan(dht_humidity) || isnan(dht_temperature)) {
-        #ifdef WLED_DEBUG_DHT
+        #ifdef DHT_DEBUG
           DEBUG_PRINTLN("---DEBUG INFO---");
           DEBUG_PRINTLN("Failed to read from DHT sensor!");
         #endif
+        Serial.println("error al leer temp");
         return;
       }
 
@@ -43,14 +50,15 @@ void userLoop(){
       char subufTemp[80];
       strcpy(subufTemp, mqttDeviceTopic);
       strcat(subufTemp, "/temperature");
-      mqtt->publish(subufTemp, 0, true, String(dht_humidity).c_str());
+      mqtt->publish(subufTemp, 0, true, String(dht_temperature).c_str());
 
       char subufHumidity[80];
       strcpy(subufHumidity, mqttDeviceTopic);
       strcat(subufHumidity, "/humidity");
-      mqtt->publish(subufHumidity, 0, true, String(dht_temperature).c_str());
+      mqtt->publish(subufHumidity, 0, true, String(dht_humidity).c_str());
       return;
     }
+    Serial.println("no conectado a mqtt");
     return;
   }
   return;
